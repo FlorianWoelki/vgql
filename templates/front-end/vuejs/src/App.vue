e4597df911b618aa3b70d7fbb54751edb7271393
florianwoelki <florianwoelki@gmx.de>
2020-06-12 11:02:19 +0200

    refactor(template): update vuejs template
    - Add working apollo to vuejs template

--- templates/front-end/vuejs/src/App.vue
+++ templates/front-end/vuejs/src/App.vue
@@ -1,17 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+      alt="Vue logo"
+      width="250"
+      src="./assets/logo.png"
+    >
+    <HelloWorld msg="Welcome to your Vue.js GraphQL App" />
+    <UserList
+      v-if="users"
+      :users="users.data"
+    />
   </div>
 </template>
 
 <script>
+import gql from 'graphql-tag';
 import HelloWorld from './components/HelloWorld.vue';
+import UserList from './components/UserList.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
+    UserList,
+  },
+  apollo: {
+    users: gql`
+      query {
+        users {
+          data {
+            id
+            name
+            username
+            email
+            phone
+            website
+          }
+        }
+      }
+    `,
   },
 };
 </script>
---
+++ templates/front-end/vuejs/src/components/UserList.vue
@@ -0,0 +1,43 @@
+<template>
+  <table>
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Username</th>
+      <th>Email</th>
+      <th>Phone</th>
+      <th>Website</th>
+    </tr>
+    <tr
+      v-for="user in users"
+      :key="user.id"
+    >
+      <td>{{ user.id }}</td>
+      <td>{{ user.name }}</td>
+      <td>{{ user.username }}</td>
+      <td>{{ user.email }}</td>
+      <td>{{ user.phone }}</td>
+      <td>{{ user.website }}</td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  props: {
+    users: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+table {
+  margin-top: 3em;
+  width: 75%;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
