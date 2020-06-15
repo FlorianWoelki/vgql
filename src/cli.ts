2c01ff158bcf0b20e4b358740394d44d0383dc56
florianwoelki <florianwoelki@gmx.de>
2020-06-15 09:41:51 +0200

    style: refactor questions import

--- src/cli.ts
+++ src/cli.ts
@@ -2,10 +2,7 @@ import * as inquirer from 'inquirer';
 import * as fs from 'fs';
 // import execa from 'execa';
 import { runTasks } from './tasks';
-import projectNameQuestion from './questions/projectName';
-import frontendChoicesQuestion from './questions/frontendChoices';
-
-const QUESTIONS = [frontendChoicesQuestion, projectNameQuestion];
+import questions from './questions';
 
 const CURR_DIR = process.cwd();
 
@@ -15,7 +12,7 @@ async function mainProcess(useDefaultAnswers: boolean, projectName?: string) {
     let inputProjectName = projectName;
 
     if (!inputProjectName) {
-      const inputValue = await inquirer.prompt([projectNameQuestion]);
+      const inputValue = await inquirer.prompt([questions.projectNameQuestion]);
       const newProjectName = inputValue['project-name'] as string;
       inputProjectName = newProjectName;
     }
@@ -24,18 +21,16 @@ async function mainProcess(useDefaultAnswers: boolean, projectName?: string) {
     fs.mkdirSync(destination);
     runTasks(templatePath, destination, inputProjectName!);
   } else {
-    inquirer
-      .prompt(QUESTIONS)
-      .then(async (answers: Record<string, unknown>) => {
-        const projectChoice = answers['frontend-choice'] as string;
-        const projectName = answers['project-name'] as string;
-        const templatePath = `${__dirname}/../templates/front-end/${projectChoice}`;
+    inquirer.prompt(questions.allQuestions).then(async (answers: any) => {
+      const projectChoice = answers['frontend-choice'] as string;
+      const projectName = answers['project-name'] as string;
+      const templatePath = `${__dirname}/../templates/front-end/${projectChoice}`;
 
-        const destination = `${CURR_DIR}/${projectName}`;
-        fs.mkdirSync(destination);
+      const destination = `${CURR_DIR}/${projectName}`;
+      fs.mkdirSync(destination);
 
-        runTasks(templatePath, destination, projectName);
-      });
+      runTasks(templatePath, destination, projectName);
+    });
   }
 }
 
---
+++ src/questions/index.ts
@@ -0,0 +1,7 @@
+import projectNameQuestion from './projectName';
+import frontendChoicesQuestion from './frontendChoices';
+
+export default {
+  projectNameQuestion,
+  allQuestions: [frontendChoicesQuestion, projectNameQuestion],
+};
