2a761f70ee0e3f1bf087f7522afaa6ea96de9bb9
florianwoelki <florianwoelki@gmx.de>
2020-06-17 09:40:26 +0200

    feat(backend): bump to last commit

--- templates/back-end/graphql/src/index.js
+++ templates/back-end/graphql/src/index.js
@@ -1,17 +1,51 @@
+import 'dotenv/config';
 import { ApolloServer, gql } from 'apollo-server-express';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors({
+  origin:
+    process.env.NODE_ENV === 'test'
+    ? '*'
+    : process.env.FRONTEND_HOST,
+  credentials: true,
+}));
 
 const typeDefs = gql`
   type Query {
-    hello: String!
+    users: [User!]!
+  }
+  type User {
+    id: Int
+    username: String
+    name: String
+    email: String
+    phone: String
+    website: String
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'hello',
+    users: () => [
+      {
+        id: 1,
+        username: 'Bret',
+        name: 'Bret Stinksi',
+        email: 'Sincere@april.biz',
+        phone: '1-770-736-8031 x56442',
+        website: 'hildegard.org',
+      },
+      {
+        id: 2,
+        username: 'Antonette',
+        name: 'Ervin Howell',
+        email: 'Shanna@melissa.tv',
+        phone: '010-692-6593 x09125',
+        website: 'anastasia.net',
+      }
+    ],
   },
 };
 
@@ -22,6 +56,7 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 
-app.listen({ port: 4000 }, () => {
+const port = process.env.PORT || 4000;
+app.listen({ port }, () => {
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
 });
