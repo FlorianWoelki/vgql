500f50c4facff8247e3fa166715ffb267bacf207
florianwoelki <florianwoelki@gmx.de>
2020-06-19 12:02:50 +0200

    feat(typescript): add ts templates

--- templates/TypeScript/front-end/vuejs/src/questions/index.spec.ts
+++
@@ -1,8 +0,0 @@
-import questions from './';
-
-describe('Questions', () => {
-  it('should export questions', () => {
-    const questionsNameOrder = Object.keys(questions);
-    expect(questionsNameOrder).toEqual(['projectNameQuestion', 'allQuestions']);
-  });
-});
--- templates/TypeScript/front-end/vuejs/templates/TypeScript/front-end/vuejs/.prettierrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  semi: true,
-  trailingComma: 'all',
-  printWidth: 100,
-  singleQuote: true,
-};
