60672c8ba4b8a2d74720ce6287df1ae363422be2
florianwoelki <florianwoelki@gmx.de>
2020-06-29 11:10:48 +0200

    fix: cors issue for ts backend

--- templates/TypeScript/back-end/graphql/src/index.ts
+++ templates/TypeScript/back-end/graphql/src/index.ts
@@ -1,11 +1,20 @@
+import 'dotenv/config';
 import 'reflect-metadata';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
+import cors from 'cors';
 import { UserResolver } from './resolvers/UserResolver';
 
 (async () => {
   const app = express();
+  app.use(cors({
+    origin:
+      process.env.NODE_ENV === 'test'
+      ? '*'
+      : process.env.FRONTEND_HOST,
+    credentials: true,
+  }));
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
