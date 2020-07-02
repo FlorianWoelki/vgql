0a801bcbc9f5eac419a6df1d8af30069f0bc6dd1
florianwoelki <florianwoelki@gmx.de>
2020-07-02 09:22:31 +0200

    feat(util): add before line insertion

--- src/transformer/typorm.ts
+++ src/transformer/typorm.ts
@@ -1,6 +1,7 @@
 import { appendToFile, appendLineToFile, readFromFile } from '../util';
 
 export default (destination: string, typeormPath: string) => {
+  // Setup Typeorm in package.json and index file
   appendToFile(
     `${destination}/server`,
     'package.json',
@@ -16,5 +17,54 @@ export default (destination: string, typeormPath: string) => {
     `${destination}/server/src/index.ts`,
     'const app = express()',
     '\n\tawait createConnection();',
+  );
+
+  // Modify User with TypeORM columns
+  appendLineToFile(
+    `${destination}/server/src/entity/User.ts`,
+    'import { ObjectType, Field, ID }',
+    "import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';",
+  );
+  appendLineToFile(
+    `${destination}/server/src/entity/User.ts`,
+    '@ObjectType()',
+    "@Entity('users')",
+    true,
+  );
+  appendLineToFile(
+    `${destination}/server/src/entity/User.ts`,
+    'id: number',
+    '\t@PrimaryGeneratedColumn()',
+    true,
+  );
+  appendLineToFile(
+    `${destination}/server/src/entity/User.ts`,
+    'username: string',
+    '\t@Column()',
+    true,
+  );
+  appendLineToFile(
+    `${destination}/server/src/entity/User.ts`,
+    'name: string',
+    '\t@Column()',
+    true,
+  );
+  appendLineToFile(
+    `${destination}/server/src/entity/User.ts`,
+    'email: string',
+    '\t@Column()',
+    true,
+  );
+  appendLineToFile(
+    `${destination}/server/src/entity/User.ts`,
+    'phone: string',
+    '\t@Column()',
+    true,
+  );
+  appendLineToFile(
+    `${destination}/server/src/entity/User.ts`,
+    'website: string',
+    '\t@Column()',
+    true,
   );
 };
