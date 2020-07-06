d8d0827ce5046507141046febd53419a7e486abe
florianwoelki <florianwoelki@gmx.de>
2020-07-06 09:43:40 +0200

    feat(view): add used techstack

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,76 @@
 <template>
-  <div class="bg-gray-200">
-    <div class="relative container pb-12 pt-12">
-      <div class="translate-l-full absolute bottom-4 left-4">
-        <TriangleSVG />
-      </div>
-      <div class="xs-hide md-hide translate-r-full absolute top-8 right-24">
-        <RectangleSVG />
-      </div>
-      <div class="row align-center p-0">
-        <div class="col-5 p-0">
-          <h1>VGQL</h1>
-          <p class="font-20">
-            The
-            <span class="underline-indigo">easy way</span>
-            to
-            <span class="underline-indigo">create</span>
-            your next Vue.js GraphQL project.
-          </p>
-          <button class="mt-1-5 mb-1-5">
-            Create your project
-          </button>
+  <section>
+    <div class="bg-gray-200">
+      <div class="relative container pb-12 pt-12 mb-0">
+        <div class="translate-l-full absolute bottom-4 left-4">
+          <TriangleSVG />
         </div>
-        <div class="col-6 offset-1 p-0">
-          <div class="relative bg-gray-900 b-radius-md p-2 pt-4 pb-4">
-            <div class="absolute top-0-75 left-0-75 bg-red-600 w-0-75 h-0-75 circle" />
-            <div class="yellow-dot absolute top-0-75 left-2 w-0-75 h-0-75 circle" />
-            <div class="red-dot absolute top-0-75 bg-green-600 w-0-75 h-0-75 circle" />
-            <code class="bl-0 bg-none bg-gray-900 text-gray-200">
-              <CodeAnimation />
-            </code>
+        <div class="xs-hide md-hide translate-r-full absolute top-8 right-24">
+          <RectangleSVG />
+        </div>
+        <div class="row align-center p-0">
+          <div class="col-5 p-0">
+            <h1>VGQL</h1>
+            <p class="font-20">
+              The
+              <span class="underline-indigo">easy way</span>
+              to
+              <span class="underline-indigo">create</span>
+              your next Vue.js GraphQL project.
+            </p>
+            <button class="mt-1-5 mb-1-5">
+              Create your project
+            </button>
+          </div>
+          <div class="col-6 offset-1 p-0">
+            <div class="relative bg-gray-900 b-radius-md p-2 pt-4 pb-4">
+              <div class="absolute top-0-75 left-0-75 bg-red-600 w-0-75 h-0-75 circle" />
+              <div class="yellow-dot absolute top-0-75 left-2 w-0-75 h-0-75 circle" />
+              <div class="red-dot absolute top-0-75 bg-green-600 w-0-75 h-0-75 circle" />
+              <code class="bl-0 bg-none bg-gray-900 text-gray-200">
+                <CodeAnimation />
+              </code>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+
+    <div class="bg-gray-300">
+      <div class="container pt-3 pb-3">
+        <h4 class="text-center mt-0 mb-0 text-gray-500 font-normal">
+          VGQL supports this tech
+        </h4>
+
+        <div class="row">
+          <div class="col-3 center mt-2">
+            <img
+              src="@/assets/tech/vuejs.png"
+              alt="vuejs"
+            >
+          </div>
+          <div class="col-3 center mt-2">
+            <img
+              src="@/assets/tech/graphql.png"
+              alt="vuejs"
+            >
+          </div>
+          <div class="col-3 center mt-2">
+            <img
+              src="@/assets/tech/typescript.png"
+              alt="vuejs"
+            >
+          </div>
+          <div class="col-3 center mt-2">
+            <img
+              src="@/assets/tech/javascript.png"
+              alt="vuejs"
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -67,4 +104,14 @@ export default {
   transform: translateX(-100%);
 }
 
+img {
+  width: 100px;
+  height: 100px;
+  -webkit-filter: grayscale(100%);
+  -moz-filter: grayscale(100%);
+  -o-filter: grayscale(100%);
+  -ms-filter: grayscale(100%);
+  filter: grayscale(100%);
+  opacity: 0.333;
+}
 </style>
