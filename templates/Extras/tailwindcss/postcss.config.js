9031f4bfaaab3422ac55b9f9add8282cffa6be84
florianwoelki <florianwoelki@gmx.de>
2020-07-27 09:25:18 +0200

    feat(templates): add tailwind as extra choice

---
+++ templates/Extras/tailwindcss/postcss.config.js
@@ -0,0 +1,14 @@
+const purgecss = require('@fullhuman/postcss-purgecss');
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+
+    process.env.NODE_ENV === 'production'
+      ? purgecss({
+        content: ['./src/**/*.html', './src/**/*.vue'],
+      })
+      : '',
+  ],
+};
