f98759a94aecaf9c67697242c1d6c7439ce9adb2
florianwoelki <florianwoelki@gmx.de>
2020-07-28 12:16:50 +0200

    test: add extra choices test

---
+++ src/questions/extraChoices.spec.ts
@@ -0,0 +1,12 @@
+import extraChoices from './extraChoices';
+
+describe('Ask for extra choices', () => {
+  it('should return correct question format', () => {
+    const result = extraChoices;
+
+    expect(result).toMatchObject({
+      type: 'checkbox',
+      name: 'extra-choices',
+    });
+  });
+});
